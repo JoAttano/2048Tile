f99769498b5911b766e96a1c912e9c3c441a545d
Jordan Batais <jordan.batais@hotmail.fr>
2017-04-26 01:06:36 +0200

    Ajoutez des fichiers projet.

---
+++ 2048Tile/Game1.cs
@@ -0,0 +1,110 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+
+namespace _2048Tile
+{
+    /// <summary>
+    /// This is the main type for your game.
+    /// </summary>
+    public class Game1 : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        GameArray gameArray;
+
+        private SpriteFont font;
+
+        private const int margeY = 100;
+        private const int margeX = 100;
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            gameArray = new GameArray();
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+            gameArray.Initialize();
+            base.Initialize();
+            
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: use this.Content to load your game content here
+            font = Content.Load<SpriteFont>("ArielFont"); //
+            gameArray.LoadContent();
+
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// game-specific content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+            gameArray.UnloadContent();
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+            gameArray.Update(gameTime);
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // TODO: Add your drawing code here
+
+            spriteBatch.Begin();
+
+            for (int i = 0; i < gameArray.Array.Length; i++)
+            {
+                
+                int tile = gameArray.Array[i / 4, i - (i / 4 * 4)];
+                Vector2 position = new Vector2(margeX + (margeX * ( i % 4)), margeY + (margeY * (int)(i / 4)));
+                System.Diagnostics.Debug.WriteLine(i);
+                spriteBatch.DrawString(font, tile.ToString() ,position,Color.Black);
+            }
+
+            spriteBatch.End();
+
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ 2048Tile/GameArray.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2048Tile
+{
+    
+    class GameArray
+    {
+        private Random rng = new Random((int)DateTime.Now.Ticks);
+        private KeyboardState newState = Keyboard.GetState();
+        private KeyboardState oldState;
+        public int[,] Array;
+
+        public GameArray()
+        {
+            Array = new int[4, 4];
+        }
+
+        public void Initialize()
+        {
+            for (int n = 0; n < Array.Length; n++)
+            {
+                Array[(int)n / 4, n - (n / 4 * 4)] = 0;
+            }
+        }
+
+        public void LoadContent()
+        {
+            generationTile();
+            generationTile();
+        }
+
+        public void UnloadContent()
+        {
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            if (oldState.IsKeyUp(Keys.Up) && newState.IsKeyDown(Keys.Up))
+            {
+
+            }
+        }
+
+        public void Draw(GameTime gameTime)
+        {
+        }
+
+        private void generationTile()
+        {
+            int x, y;
+            bool tileValid = false;
+
+            while (!tileValid)
+            {
+                x = rng.Next(4);
+                y = rng.Next(4);
+
+                if (Array[y, x] == 0)
+                {
+                    Array[y, x] = (rng.Next(1)*2) + 2;
+                    tileValid = !tileValid;
+                }
+            }
+        }
+
+
+
+        enum Direction { up, down, right, left };
+    }
+}
